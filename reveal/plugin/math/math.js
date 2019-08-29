9b0229658117189d216f8521b651ab21de8e7535
jbcazaux <jbcazaux@gmail.com>
2019-08-29 17:06:38 +0200

    update BBL

--- reveal/plugin/math/math.js
+++ reveal/plugin/math/math.js
@@ -4,59 +4,89 @@
  *
  * @author Hakim El Hattab
  */
-var RevealMath =
-  window.RevealMath ||
-  (function() {
-    var options = Reveal.getConfig().math || {}
-    options.mathjax = options.mathjax || 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js'
-    options.config = options.config || 'TeX-AMS_HTML-full'
-    options.tex2jax = options.tex2jax || {
-      inlineMath: [['$', '$'], ['\\(', '\\)']],
-      skipTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
-    }
+var RevealMath = window.RevealMath || (function(){
 
-    loadScript(options.mathjax + '?config=' + options.config, function() {
-      MathJax.Hub.Config({
-        messageStyle: 'none',
-        tex2jax: options.tex2jax,
-        skipStartupTypeset: true,
-      })
+	var options = Reveal.getConfig().math || {};
+	var mathjax = options.mathjax || 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js';
+	var config = options.config || 'TeX-AMS_HTML-full';
+	var url = mathjax + '?config=' + config;
 
-      // Typeset followed by an immediate reveal.js layout since
-      // the typesetting process could affect slide height
-      MathJax.Hub.Queue(['Typeset', MathJax.Hub])
-      MathJax.Hub.Queue(Reveal.layout)
+	var defaultOptions = {
+		messageStyle: 'none',
+		tex2jax: {
+			inlineMath: [ [ '$', '$' ], [ '\\(', '\\)' ] ],
+			skipTags: [ 'script', 'noscript', 'style', 'textarea', 'pre' ]
+		},
+		skipStartupTypeset: true
+	};
 
-      // Reprocess equations in slides when they turn visible
-      Reveal.addEventListener('slidechanged', function(event) {
-        MathJax.Hub.Queue(['Typeset', MathJax.Hub, event.currentSlide])
-      })
-    })
+	function defaults( options, defaultOptions ) {
 
-    function loadScript(url, callback) {
-      var head = document.querySelector('head')
-      var script = document.createElement('script')
-      script.type = 'text/javascript'
-      script.src = url
+		for ( var i in defaultOptions ) {
+			if ( !options.hasOwnProperty( i ) ) {
+				options[i] = defaultOptions[i];
+			}
+		}
 
-      // Wrapper for callback to make sure it only fires once
-      var finish = function() {
-        if (typeof callback === 'function') {
-          callback.call()
-          callback = null
-        }
-      }
+	}
 
-      script.onload = finish
+	function loadScript( url, callback ) {
 
-      // IE
-      script.onreadystatechange = function() {
-        if (this.readyState === 'loaded') {
-          finish()
-        }
-      }
+		var head = document.querySelector( 'head' );
+		var script = document.createElement( 'script' );
+		script.type = 'text/javascript';
+		script.src = url;
 
-      // Normal browsers
-      head.appendChild(script)
-    }
-  })()
+		// Wrapper for callback to make sure it only fires once
+		var finish = function() {
+			if( typeof callback === 'function' ) {
+				callback.call();
+				callback = null;
+			}
+		}
+
+		script.onload = finish;
+
+		// IE
+		script.onreadystatechange = function() {
+			if ( this.readyState === 'loaded' ) {
+				finish();
+			}
+		}
+
+		// Normal browsers
+		head.appendChild( script );
+
+	}
+
+	return {
+		init: function() {
+
+			defaults( options, defaultOptions );
+			defaults( options.tex2jax, defaultOptions.tex2jax );
+			options.mathjax = options.config = null;
+
+			loadScript( url, function() {
+
+				MathJax.Hub.Config( options );
+
+				// Typeset followed by an immediate reveal.js layout since
+				// the typesetting process could affect slide height
+				MathJax.Hub.Queue( [ 'Typeset', MathJax.Hub ] );
+				MathJax.Hub.Queue( Reveal.layout );
+
+				// Reprocess equations in slides when they turn visible
+				Reveal.addEventListener( 'slidechanged', function( event ) {
+
+					MathJax.Hub.Queue( [ 'Typeset', MathJax.Hub, event.currentSlide ] );
+
+				} );
+
+			} );
+
+		}
+	}
+
+})();
+
+Reveal.registerPlugin( 'math', RevealMath );
--- reveal/plugin/notes-server/client.js
+++ reveal/plugin/notes-server/client.js
@@ -1,65 +1,65 @@
-;(function() {
-  // don't emit events from inside the previews themselves
-  if (window.location.search.match(/receiver/gi)) {
-    return
-  }
+(function() {
 
-  var socket = io.connect(window.location.origin),
-    socketId = Math.random()
-      .toString()
-      .slice(2)
+	// don't emit events from inside the previews themselves
+	if( window.location.search.match( /receiver/gi ) ) { return; }
 
-  console.log('View slide notes at ' + window.location.origin + '/notes/' + socketId)
+	var socket = io.connect( window.location.origin ),
+		socketId = Math.random().toString().slice( 2 );
 
-  window.open(window.location.origin + '/notes/' + socketId, 'notes-' + socketId)
+	console.log( 'View slide notes at ' + window.location.origin + '/notes/' + socketId );
 
-  /**
-   * Posts the current slide data to the notes window
-   */
-  function post() {
-    var slideElement = Reveal.getCurrentSlide(),
-      notesElement = slideElement.querySelector('aside.notes')
+	window.open( window.location.origin + '/notes/' + socketId, 'notes-' + socketId );
 
-    var messageData = {
-      notes: '',
-      markdown: false,
-      socketId: socketId,
-      state: Reveal.getState(),
-    }
+	/**
+	 * Posts the current slide data to the notes window
+	 */
+	function post() {
 
-    // Look for notes defined in a slide attribute
-    if (slideElement.hasAttribute('data-notes')) {
-      messageData.notes = slideElement.getAttribute('data-notes')
-    }
+		var slideElement = Reveal.getCurrentSlide(),
+			notesElement = slideElement.querySelector( 'aside.notes' );
 
-    // Look for notes defined in an aside element
-    if (notesElement) {
-      messageData.notes = notesElement.innerHTML
-      messageData.markdown = typeof notesElement.getAttribute('data-markdown') === 'string'
-    }
+		var messageData = {
+			notes: '',
+			markdown: false,
+			socketId: socketId,
+			state: Reveal.getState()
+		};
 
-    socket.emit('statechanged', messageData)
-  }
+		// Look for notes defined in a slide attribute
+		if( slideElement.hasAttribute( 'data-notes' ) ) {
+			messageData.notes = slideElement.getAttribute( 'data-notes' );
+		}
 
-  // When a new notes window connects, post our current state
-  socket.on('new-subscriber', function(data) {
-    post()
-  })
+		// Look for notes defined in an aside element
+		if( notesElement ) {
+			messageData.notes = notesElement.innerHTML;
+			messageData.markdown = typeof notesElement.getAttribute( 'data-markdown' ) === 'string';
+		}
 
-  // When the state changes from inside of the speaker view
-  socket.on('statechanged-speaker', function(data) {
-    Reveal.setState(data.state)
-  })
+		socket.emit( 'statechanged', messageData );
 
-  // Monitor events that trigger a change in state
-  Reveal.addEventListener('slidechanged', post)
-  Reveal.addEventListener('fragmentshown', post)
-  Reveal.addEventListener('fragmenthidden', post)
-  Reveal.addEventListener('overviewhidden', post)
-  Reveal.addEventListener('overviewshown', post)
-  Reveal.addEventListener('paused', post)
-  Reveal.addEventListener('resumed', post)
+	}
 
-  // Post the initial state
-  post()
-})()
+	// When a new notes window connects, post our current state
+	socket.on( 'new-subscriber', function( data ) {
+		post();
+	} );
+
+	// When the state changes from inside of the speaker view
+	socket.on( 'statechanged-speaker', function( data ) {
+		Reveal.setState( data.state );
+	} );
+
+	// Monitor events that trigger a change in state
+	Reveal.addEventListener( 'slidechanged', post );
+	Reveal.addEventListener( 'fragmentshown', post );
+	Reveal.addEventListener( 'fragmenthidden', post );
+	Reveal.addEventListener( 'overviewhidden', post );
+	Reveal.addEventListener( 'overviewshown', post );
+	Reveal.addEventListener( 'paused', post );
+	Reveal.addEventListener( 'resumed', post );
+
+	// Post the initial state
+	post();
+
+}());
